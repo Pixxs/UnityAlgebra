9aafc2ce56efda50547796d79b387df1c917e150
aeybelvarghese@gmail.com <aeybelvarghese@gmail.com>
2018-12-02 16:00:41 -0600

    Fixed menu animation

--- Hot JepJep/Assets/Scripts/AnimatedTextMenu.cs
+++ Hot JepJep/Assets/Scripts/AnimatedTextMenu.cs
@@ -4,27 +4,49 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class AnimatedTextMenu : MonoBehaviour {
+    public bool mainMenu;
     public GameObject TitleText;
     public GameObject StartButton;
+    public GameObject Dice;
 	// Use this for initialization
 	void Start () {
-        TitleText.GetComponent<TextMesh>().color = Color.HSVToRGB(0f,0.7f,1f);
-        var initButtonColors = StartButton.GetComponent<Button>().colors;
-        initButtonColors.normalColor = Color.HSVToRGB(0f, 0.7f, 1f);
-        StartButton.GetComponent<Button>().colors = initButtonColors;
+        if (mainMenu)
+        {
+            TitleText.GetComponent<TextMesh>().color = Color.HSVToRGB(0f, 0.7f, 1f);
+            var initButtonColors = StartButton.GetComponent<Button>().colors;
+            initButtonColors.normalColor = Color.HSVToRGB(0f, 0.7f, 1f);
+            StartButton.GetComponent<Button>().colors = initButtonColors;
+        }
+        else
+        {
+            Dice.GetComponent<Image>().color = Color.HSVToRGB(0f, 0.7f, 1f);
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-        var newColor = StartButton.GetComponent<Button>().colors;
-        float newHue;
-        float newJunk;
-        Color.RGBToHSV(newColor.normalColor, out newHue,out newJunk, out newJunk);
-        newHue = newHue + 0.001f;
-        
-        newColor.normalColor = Color.HSVToRGB(newHue, 0.7f, 1f);
+        if (mainMenu)
+        {
+            var newColor = StartButton.GetComponent<Button>().colors;
+            float newHue;
+            float newJunk;
+            Color.RGBToHSV(newColor.normalColor, out newHue, out newJunk, out newJunk);
+            newHue = newHue + 0.001f;
 
-        TitleText.GetComponent<TextMesh>().color = newColor.normalColor;
-        StartButton.GetComponent<Button>().colors = newColor;
+            newColor.normalColor = Color.HSVToRGB(newHue, 0.7f, 1f);
+
+            TitleText.GetComponent<TextMesh>().color = newColor.normalColor;
+            StartButton.GetComponent<Button>().colors = newColor;
+        }
+        else
+        {
+            var oldColor = Dice.GetComponent<Image>().color;
+            float newHue;
+            float newJunk;
+            Color.RGBToHSV(oldColor, out newHue, out newJunk, out newJunk);
+            newHue = newHue + 0.0025f;
+            Dice.GetComponent<Image>().color = Color.HSVToRGB(newHue, 0.7f, 1f);
+        }
+        
     }
 }
