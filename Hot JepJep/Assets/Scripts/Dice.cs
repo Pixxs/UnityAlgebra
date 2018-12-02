7b228007ae4f35498b6bb15b24901781dbff4be6
aeybelvarghese@gmail.com <aeybelvarghese@gmail.com>
2018-12-02 16:16:53 -0600

    Team names load and work on Dice

---
+++ Hot JepJep/Assets/Scripts/Dice.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour {
+	public GameObject DiceUIPanel;
+	public GameObject DiceText;
+	// Use this for initialization
+	public int[] DiceStuff;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Hot JepJep/Assets/Scripts/TeamNameLoader.cs
+++ Hot JepJep/Assets/Scripts/TeamNameLoader.cs
@@ -5,35 +5,24 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TeamNameLoader : MonoBehaviour {
-
-	public string Team1Name;
-	public string Team2Name;
-	public string Team3Name;
-	public string Team4Name;
-	public string Team5Name;
-	public Text Team1;
-	public Text Team2;
-	public Text Team3;
-	public Text Team4;
-	public Text Team5;
+	public Text Team1Text;
+	public Text Team2Text;
+	public Text Team3Text;
+	public Text Team4Text;
+	public Text Team5Text;
 
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-				Team1Name = PlayerPrefs.GetString ("Team1NameSave");
-				Team2Name = PlayerPrefs.GetString ("Team2NameSave");
-				Team3Name = PlayerPrefs.GetString ("Team3NameSave");
-				Team4Name = PlayerPrefs.GetString ("Team4NameSave");
-				Team5Name = PlayerPrefs.GetString ("Team5NameSave");
-				Team1.text = Team1Name;
-				Team2.text = Team2Name;
-				Team3.text = Team3Name;
-				Team4.text = Team4Name;
-				Team5.text = Team5Name;
+				Team1Text.text= PlayerPrefs.GetString ("Team1NameSave");
+				Team2Text.text= PlayerPrefs.GetString ("Team2NameSave");
+				Team3Text.text= PlayerPrefs.GetString ("Team3NameSave");
+				Team4Text.text= PlayerPrefs.GetString ("Team4NameSave");
+				Team5Text.text= PlayerPrefs.GetString ("Team5NameSave");
 			}
 	}
