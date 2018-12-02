96468f581b6df338fe08260f90d114bf9f41aea4
aeybelvarghese@gmail.com <aeybelvarghese@gmail.com>
2018-12-02 13:48:57 -0600

    Revamped Main Menu

---
+++ Hot JepJep/Assets/Scripts/AnimatedTextMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedTextMenu : MonoBehaviour {
+    public GameObject TitleText;
+    public GameObject StartButton;
+	// Use this for initialization
+	void Start () {
+        TitleText.GetComponent<TextMesh>().color = Color.HSVToRGB(0f,0.7f,1f);
+        var initButtonColors = StartButton.GetComponent<Button>().colors;
+        initButtonColors.normalColor = Color.HSVToRGB(0f, 0.7f, 1f);
+        StartButton.GetComponent<Button>().colors = initButtonColors;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var newColor = StartButton.GetComponent<Button>().colors;
+        float newHue;
+        float newJunk;
+        Color.RGBToHSV(newColor.normalColor, out newHue,out newJunk, out newJunk);
+        newHue = newHue + 0.001f;
+        
+        newColor.normalColor = Color.HSVToRGB(newHue, 0.7f, 1f);
+
+        TitleText.GetComponent<TextMesh>().color = newColor.normalColor;
+        StartButton.GetComponent<Button>().colors = newColor;
+    }
+}
